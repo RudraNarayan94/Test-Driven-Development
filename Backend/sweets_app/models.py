6fce0278b8ccf1703fd687b0d1e6d64167e15ab8
Rudra <rudranarayans10@gmail.com>
2025-09-19 23:23:48 +0530

    imple: sweet model

--- Backend/sweets_app/models.py
+++ Backend/sweets_app/models.py
@@ -3,3 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
+
+class Sweet(models.Model):
+    
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity_in_stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
